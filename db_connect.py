b180007ff3a780fc47d195389b668f3db10b5cff
Balamurugan T <balamuruganthangvelu@gmail.com>
2026-07-24 17:45:05 +0530

    update neon

--- db_connect.py
+++ db_connect.py
@@ -1,21 +1,45 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def get_connection():
+
     try:
         connection = psycopg2.connect(
+
             host=os.getenv("DB_HOST"),
-            database="startup_funding",
-            user="postgres",
-            password="*******"
+
+            database=os.getenv("DB_NAME"),
+
+            user=os.getenv("DB_USER"),
+
+            password=os.getenv("DB_PASSWORD"),
+
+            port=os.getenv("DB_PORT", "5432")
         )
+
         print("Connection established successfully")
+
         return connection
+
     except psycopg2.Error as e:
+
         print(f"Error connecting to the database: {e}")
+
         return None
+
+
 if __name__ == "__main__":
+
     conn = get_connection()
 
     if conn:
+
         print("Database connection is working!")
+
         conn.close()
+
         print("Connection closed.")
